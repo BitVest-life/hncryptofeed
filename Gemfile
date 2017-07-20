874d03cc72daedb08edd3095349faba0937f2231
Marcio Klepacz <marcioklepacz@gmail.com>
2017-07-20 15:27:25 -0300

    inital commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.4.1'
+
+gem 'sinatra'
+gem 'feedjira'
+gem 'httparty'
+gem 'puma'
---
+++ app.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+require 'rss'
+require 'httparty'
+require 'feedjira'
+
+KEYWORDS = [
+  'ethereum',
+  'ether',
+  'cryptocurrency',
+  'cryptocurrencies',
+  'bitcoin', 
+  'litecoin',
+  'ico',
+  'coin',
+  'blockchain',
+  'segwit'
+]
+
+class HNCryptoFeed < Sinatra::Base 
+
+  configure do
+    set :keywords, YAML.load_file('keywords.yaml')['keywords']
+  end
+
+  get '/' do
+    url = 'https://news.ycombinator.com/rss'
+    xml = HTTParty.get(url).body
+    return 404 unless xml and xml != ''
+    feed = Feedjira::Feed.parse(xml)
+    filtered_feed = feed.entries.map do |entry|
+      words = entry.title.downcase.split(' ') 
+      settings.keywords.map { |keyword| entry if words.include?(keyword) }
+    end
+    entries = filtered_feed.flatten.compact
+
+    rss = RSS::Maker.make('2.0') do |maker|
+      maker.channel.author = 'Marcio Klepacz'
+      maker.channel.link = request.url
+      maker.channel.title = 'HN Crypto'
+      maker.channel.description = 'Hacker News filter for cryptocurrencies related news'
+      maker.channel.updated = Time.now.to_s
+
+      entries.each do |entry|
+        maker.items.new_item do |item|
+          item.link = entry.url
+          item.title =  entry.title
+          item.updated = entry.published
+        end
+      end
+    end
+
+    rss.to_s
+  end
+end
+
+
